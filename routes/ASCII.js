f9a61e435521055cba84939740f0b1662a971818
StevenMM23 <esteven123123@outlook.com>
2022-11-15 10:08:46 -0400

    Add ASCII To Character

--- routes/ASCII.js
+++ routes/ASCII.js
@@ -6,6 +6,9 @@ const obtainASCII = (a) => {
   return a.charCodeAt(0);
 }
 
+const obtainCharacter = (a) => { 
+  return String.fromCharCode(a);
+}
 
 router.get("/abc", (req, res) => {
   res.render("../views/CharToASCII.ejs", {previewValue : "", ascii : "", title: "Character to ASCII"});
@@ -18,5 +21,14 @@ router.post("/CharToASCII/Result", (req, res) => {
 
   res.render("../views/CharToASCII.ejs", { previewValue: CharacterValue,  ascii, title: "Character to ASCII"});
 });
+
+router.get("/ASCIIChar", (req, res) => { 
+  res.render("../views/ASCIIChar.ejs", {title: "ASCII to Character" , previewValue: "" , character: ""})
+})
+router.post("/ASCIIChar/Result", (req, res) => { 
+  const AsciiValue = req.body.AsciiValue
+  const CharacterValue = obtainCharacter(AsciiValue);
+  res.render("../views/ASCIIChar.ejs", {title: "ASCII to Character" , previewValue: AsciiValue , character: CharacterValue })
+})
 
 module.exports = router;
