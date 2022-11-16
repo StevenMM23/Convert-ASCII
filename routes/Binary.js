9a5d01e3e56817e920852429efd83f3e472ee1c7
StevenMM23 <esteven123123@outlook.com>
2022-11-16 15:58:33 -0400

    Add Async Function

--- routes/Binary.js
+++ routes/Binary.js
@@ -10,12 +10,10 @@ router.post("/DecimalToBinary/Result", async (req, res, next) => {
   const decimalValue = req.body.DecimalValue;
   const url = `https://networkcalc.com/api/binary/${decimalValue.trim()}?from=10&to=2`
   try {
-    const BinaryValue = await axios
-      .get(url)
-      .then(({ data }) => {
-        return data.converted;
-      });
-      res.render("../views/DecimalBinary.ejs", ({title: "Decimal to Binary", previewValue: decimalValue, binary: BinaryValue}))
+    const BinaryValue = await axios.get(url);
+    const {data : {converted}} = BinaryValue;
+    
+    res.render("../views/DecimalBinary.ejs", ({title: "Decimal to Binary", previewValue: decimalValue, binary: converted}))
       
   } 
   catch (error) {
