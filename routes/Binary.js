e3c093dcc1a5b494406252c299e562534f1a865c
StevenMM23 <esteven123123@outlook.com>
2022-11-19 09:05:24 -0400

    Add type numbers on Input

--- routes/Binary.js
+++ routes/Binary.js
@@ -6,18 +6,16 @@ router.get("/DecimalToBinary", (req, res) => {
   res.render("../views/DecimalBinary.ejs", { title: "Decimal To Binary" , previewValue : " ", binary : " "});
 });
 
-router.post("/DecimalToBinary/Result", async (req, res, next) => {
-  const decimalValue = req.body.DecimalValue;
-  const url = `https://networkcalc.com/api/binary/${decimalValue.trim()}?from=10&to=2`
+router.post("/DecimalToBinary/Result", async (req, res) => {
+  
   try {
-    const BinaryValue = await axios.get(url);
-    const {data : {converted}} = BinaryValue;
-    
+    const decimalValue = req.body.DecimalValue;
+    const url = `https://networkcalc.com/api/binary/${decimalValue.trim()}?from=10&to=2`
+    const {data : {converted}} = await axios.get(url);
     res.render("../views/DecimalBinary.ejs", ({title: "Decimal to Binary", previewValue: decimalValue, binary: converted}))
-      
   } 
   catch (error) {
-    res.send(error)
+    res.redirect("/DecimalToBinary")
   }
 });
 
